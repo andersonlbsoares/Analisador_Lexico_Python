3b5264cdedf543a0c33b5d936adb92a46ee42b99
Anderson Soares <andersonlbsoares@alu.ufc.br>
2023-05-28 18:39:10 -0300

    up

--- teste.py
+++
@@ -1,9 +0,0 @@
-teste = ['KEYWORD_INT', 'STRING', 'IDENTIFIER']
-
-KEYWORD_INT = 'KEYWORD_INT'
-
-if KEYWORD_INT in teste:
-    print('KEYWORD_INT')
-elif 'STRING' in teste:
-    print('STRING')
-    
